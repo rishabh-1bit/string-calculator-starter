6c5184d5413900a79dcec0c10ea1c7a22473f5b7
risha <risha@LAPTOP-01CG1R63>
2021-10-16 12:58:36 +0530

    calculated string result for empty string and single int

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,10 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	if(input==null || input.isEmpty())
         return 0;
+    	else
+    		return Integer.parseInt(input);
     }
 
 }
